dc5dc4be6a211c8624da1d33cc3aa5f37c6d8eff
wfuzatto <shockwave_acb@hotmail.com>
2026-08-18 14:19:19 -0300

    Route funk to dedicated arranger

--- studio-hq/musiclite_hq/arranger.py
+++ studio-hq/musiclite_hq/arranger.py
@@ -3,16 +3,22 @@ from .midi_tools import write_midi, make_tempo_map
 from .genres import normalize_genre
 from .song_form import make_form
 from .drumgizmo import note_map
-from .musicians import hq3
+from .musicians import hq3, funk
 
 def create_test_midis(work: Path, seconds=60, bpm=126, genre="sertanejo"):
     genre=normalize_genre(genre)
     bars=max(4, round(seconds / ((60/bpm)*4)))
     form=make_form(bars,genre)
     tempo=make_tempo_map(bpm,bars,form=form)
-    drums=note_map(genre)
 
-    if genre=="rock":
+    if genre=="funk":
+        drums={}
+        specs={
+            "drums": funk.funk_drums,
+            "sub": funk.funk_sub,
+        }
+    elif genre=="rock":
+        drums=note_map(genre)
         specs={
             "drums": hq3.rock_drums,
             "bass": hq3.rock_bass,
@@ -20,6 +26,7 @@ def create_test_midis(work: Path, seconds=60, bpm=126, genre="sertanejo"):
             "guitar_r": hq3.rock_guitar_right,
         }
     else:
+        drums=note_map(genre)
         specs={
             "drums": hq3.sertanejo_drums,
             "bass": hq3.sertanejo_bass,
